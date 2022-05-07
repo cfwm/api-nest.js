8f218e467b5a3fc8fb8b6d3913565b83c8dd4039
Carlos Moreira <carlosfwm@gmail.com>
2022-05-07 00:11:07 -0300

    add user address

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -4,4 +4,14 @@ export const UserSchema = new mongoose.Schema({
   name: String,
   surname: String,
   age: Number,
+  address: {
+    cep: String,
+    state: String,
+    city: String,
+    district: String,
+    street: String,
+    number: String,
+    complement: String,
+    ibge: String,
+  },
 });
--- src/users/shared/user.ts
+++ src/users/shared/user.ts
@@ -4,4 +4,14 @@ export class User extends Document {
   name: string;
   surname: string;
   age: number;
+  address: {
+    cep: string;
+    state: string;
+    city: string;
+    district: string;
+    street: string;
+    number: string;
+    complement: string;
+    ibge: string;
+  };
 }
